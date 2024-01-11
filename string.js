f7ee2083c7d9857dce0bf84a51407a5f57497e23
p1yu5h0 <piyush200205@gmail.com>
2024-01-11 08:48:34 +0530

    first commit

---
+++ string.js
@@ -0,0 +1,13 @@
+const client = require("./client")
+
+async function init() {
+    client.set("user:5", "nikhil rawat");
+    client.setnx("user:5", "nikhit");
+    client.set("user:7", "laxman");
+    await client .expire("user:7", 10);
+    for(let i = 1; i<=7; i++){
+        const result = await client.get('user:' + i)
+        console.log(`Result -> ${result}`);
+    }
+}
+init();
